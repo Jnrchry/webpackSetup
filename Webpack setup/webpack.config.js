e55a0daf7d376ba596db67882527b9d2535c0f3c
jnrchry <jnrchry@gmail.com>
2022-12-15 02:26:20 +0000

    commit

--- Webpack setup/webpack.config.js
+++ Webpack setup/webpack.config.js
@@ -1,26 +1,5 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = resolve("path");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "./src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack Setup",
-      filename: "index.html",
-      template: "./src/template.html",
-    }),
-  ],
 };
