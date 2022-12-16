c16889070394fdab45e9d207b7ed2f0f5a845c26
jnrchry <jnrchry@gmail.com>
2022-12-16 00:21:49 +0000

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,27 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    clean: true,
+    assetModuleFilename: "src/assets/images/[name].[ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
