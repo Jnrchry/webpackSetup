1610ba0861abf310a89fc0ed8965834060d9cc67
jnrchry <jnrchry@gmail.com>
2022-12-24 01:44:10 +0000

    commit

--- Social media site/src/Components/Feed/Feed.js
+++ Social media site/src/Components/Feed/Feed.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./feed.scss";
 import Stories from "../Stories/Stories";
+import Share from "../Share/Share";
 
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Stories />
+        <Share />
       </div>
     </div>
   );
