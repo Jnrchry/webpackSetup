e10bd8e79ec55d8280b06cab0c4826e09278b70f
jnrchry <jnrchry@gmail.com>
2022-12-24 01:44:02 +0000

    commit

---
+++ Social media site/src/Components/Share/Share.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./share.scss";
+
+const Share = () => {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop"></div>
+
+        <div className="shareBottom"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
